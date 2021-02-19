2615f7f932d7522033938f6d1fcab42213160d9c
emmanuel23dms <emmanuel23dms@gmail.com>
2021-02-19 01:59:12 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+
+  require 'database.php';
+
+  if (isset($_SESSION['user_id'])) {
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
+    $records->bindParam(':id', $_SESSION['user_id']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $user = null;
+
+    if (count($results) > 0) {
+      $user = $results;
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="include/styletwo.css">
+</head>
+<body>
+
+<h1>please login or signup</h1>
+<a href="login.php">Login</a> or <a href="signup.php">Signup</a>
+
+<h2>
+<a href="indexser.php">Tienda</a>
+</h2>
+    
+</body>
+</html>
